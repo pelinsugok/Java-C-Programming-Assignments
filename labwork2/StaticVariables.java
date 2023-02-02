d2e536a43f8f68db2ac75bf070673e0833564e33
Pelin Su Gök <81471364+pelinsugok@users.noreply.github.com>
2023-02-02 23:34:44 +0300

    Delete labwork2 directory

--- labwork2/StaticVariables.java
+++
@@ -1,8 +0,0 @@
-package labwork2;
-
-public final class StaticVariables {
-	
-	public static double halfLength = 0.5;
-	public static int canvasHeight = 1000;
-	public static int canvasWidth = 1000;
-}
